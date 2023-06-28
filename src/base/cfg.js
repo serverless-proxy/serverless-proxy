af027b55f718d7ab5f41daa1ee64c6ca10efa92b
Murtaza Aliakbar <murtaza@live.in>
2023-06-29 02:11:01 +0530

    cfg: m comments

--- src/base/cfg.js
+++ src/base/cfg.js
@@ -6,14 +6,22 @@ export const debug = true;
 // echo "hello" | nc midway.fly.dev 5001
 export const echoServerAddr = { hostname: "midway.fly.dev", port: 5001 };
 export const g204Url = "https://static.googleusercontent.com/generate_204";
+// socket options on workers
 export const tcpOpts = { secureTransport: "off", allowHalfOpen: true };
 export const h2header = {
   "Content-Type": "application/octet-stream",
   "Cache-Control": "no-cache",
 };
+// auth token
 export const headerClaim = "x-nile-pip-claim";
+// msg is a nonce for the claim
 export const headerMsg = "x-nile-pip-msg";
+// dangerous. do not enable. bypass auth?
+// not respected when wenv is prod
 export const bypassAuth = true;
+// immutable info context for keygen
 export const authContext = "per-client-pip-key";
+// websockets upgrade header
 export const upgradeHeader = "Upgrade";
+// connection timeout for websockets; unused on workers
 export const wsConnTimeoutMs = 10000;
